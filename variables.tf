e4ec07fe453d1bbc9eebe1696c433a466b208aa7
ASC IT Solutions <andrew@asc-solutions.co.uk>
2021-03-03 12:37:51 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,14 @@
-variable "role_definitions" {}
-
+variable "role_definitions" {
+  type = object({
+    name        = string
+    description = string
+    scope       = string
+    assignable_scopes = list(string)
+    permissions = object({
+      actions          = list(string)
+      not_actions      = list(string)
+      data_actions     = list(string)
+      not_data_actions = list(string)
+      })
+    })
+}
