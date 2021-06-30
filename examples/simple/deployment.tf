2dd8d94b207c70509ebcb498bdf72e234664d687
Cluey <andrew@asc-solutions.co.uk>
2021-06-30 11:02:20 +0100

    Update deployment.tf

--- examples/simple/deployment.tf
+++ examples/simple/deployment.tf
@@ -8,7 +8,8 @@ provider "azurerm" {
 
 module "new_roles" {
   for_each = local.custom_role_definitions
-  source = "../../"
+   source  = "andrewCluey/custom-role/azurerm//examples/simple"
+  version = "1.3.0"
 
   role_name       = each.key
   role_definition = each.value
